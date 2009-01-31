6c6b7109fbfd1422fe220b1c7731821184d7c260
Narihiro Nakamura <authorNari@gmail.com>
2009-01-31 15:05:08 +0900

    add file

---
+++ config/initializers/init_reminders.rb
@@ -0,0 +1,18 @@
+begin
+  require 'translate_untaint'
+  require 'date'
+  require 'friendly_id'
+  
+  # tag init
+  TagList.delimiter = " "
+  Tag.destroy_unused = true
+  
+rescue Exception => ex
+  puts "\nSigmas initialization failure."
+  puts "\nPlease check config/initializers/inflections.rb"
+  puts "\nError Class:\n\n #{ex.class}"
+  puts "\nError Messages:\n\n  #{ex.message}"
+  puts "\nError Backtrace:\n\n  #{ex.backtrace.join("\n  ")}"
+  exit(1)
+end
+
