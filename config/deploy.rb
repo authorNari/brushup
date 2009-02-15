dbf9ee6f8b3b87bd52e45ab75e4c2b48a83e4c93
takaokouji <kouji@takao7.net>
2009-02-15 21:17:39 +0900

    added deploy files for HAS project.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,21 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "brushup"
+set :repository,  "git://github.com/takaokouji/forget_me_not.git"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/var/lib/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
-# set :scm, :subversion
+set :scm, :git
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+set :branch, "master"
+
+set :deploy_via, :export
+
+set :use_sudo, false
+
+default_run_options[:shell] = false
+
+set :user, application
