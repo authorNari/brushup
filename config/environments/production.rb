29950ad12ec5dc0a72bd71e91f6c9144800e1fc0
Narihiro Nakamura <authornari@gmail.com>
2012-10-18 17:28:21 +0900

    not use assets:precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,13 +12,13 @@ Brushup::Application.configure do
   config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
   # Generate digests for assets URLs
-  config.assets.digest = true
+  config.assets.digest = false
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
