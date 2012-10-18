56a80da772c6724838b16eb31e7826d8885577bd
Narihiro Nakamura <authornari@gmail.com>
2012-10-18 19:12:25 +0900

    Revert "not use assets pipeline"
    This reverts commit c91b30526defdb04d14daf31d90c89cb3a73a80a.

--- config/application.rb
+++ config/application.rb
@@ -56,7 +56,7 @@ module Brushup
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = false
+    config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
