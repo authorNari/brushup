c91b30526defdb04d14daf31d90c89cb3a73a80a
Narihiro Nakamura <authornari@gmail.com>
2012-10-18 17:40:18 +0900

    not use assets pipeline

--- config/application.rb
+++ config/application.rb
@@ -56,7 +56,7 @@ module Brushup
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+    config.assets.enabled = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
