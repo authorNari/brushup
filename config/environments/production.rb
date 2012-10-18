ad314dc88a25e7e5618d60e3bdc8395456d41fd1
Narihiro Nakamura <authornari@gmail.com>
2012-10-18 19:12:59 +0900

    Revert "not use assets:precompile"
    This reverts commit 29950ad12ec5dc0a72bd71e91f6c9144800e1fc0.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,13 +12,13 @@ Brushup::Application.configure do
   config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
-  config.assets.compress = false
+  config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
   # Generate digests for assets URLs
-  config.assets.digest = false
+  config.assets.digest = true
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
